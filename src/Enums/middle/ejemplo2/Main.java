ae83cac73add34eaf6773053f55b5824b2250e8e
Sadissxz <89706963+SadisOfc@users.noreply.github.com>
2025-07-26 21:04:21 -0500

    fix: ejemplo 2 - Main

--- src/Enums/middle/ejemplo2/Main.java
+++ src/Enums/middle/ejemplo2/Main.java
@@ -1,14 +1,12 @@
 package Enums.middle.ejemplo2;
 
-import java.lang.Math;
-
 public class Main {
     public static void main(String[]args){
         double a = 284;
         double b = 2;
-        System.out.println(java.lang.Math.SUM.apply(a,b));
-        System.out.println(java.lang.Math.SUBTRACT.apply(a,b));
-        System.out.println(java.lang.Math.DIVIDE.apply(a,b));
+        System.out.println(Math.SUM.apply(a,b));
+        System.out.println(Math.SUBTRACT.apply(a,b));
+        System.out.println(Math.DIVIDE.apply(a,b));
         System.out.println(Math.MULTIPLY.apply(a,b));
     }
 }
